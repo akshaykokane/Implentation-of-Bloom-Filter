f19f4f97a577716cfb243c99f451fa5ba420ae30
Akshay Kokane <30575487+akshaykokane@users.noreply.github.com>
2019-10-07 16:53:41 -0400

    -updated

--- Bloom_Client/src/Client.java
+++ Bloom_Client/src/Client.java
@@ -14,33 +14,37 @@ public class Client {
 		 //read the input from the file
 		 Registry registry = LocateRegistry.getRegistry(8080);
          BloomFilterInterface stub = (BloomFilterInterface) registry.lookup("bloomfilter");
-		 File inputFile = new File("/Users/akshaykokane/Documents/eclipse-workplace/Bloom_Client/src/inputFile.txt");
+		 File inputFile = new File("/Users/akshaykokane/Documents/eclipse-workplace/Bloom_Client/src/inputFile2.txt");
 	  	 BufferedReader br = new BufferedReader(new FileReader(inputFile));
 	  	 String st;
 	  	 float falsePositiveCount = 0;
 	     float numberOfInputs = 0;
+	     float numberOfTestInputs = 0;
 	  	 while ((st = br.readLine()) != null) {
 	  		
 	  		 if(st.contains("add")) {
-	  			stub.add(st.split(" ")[0]);
+	  			stub.add(st.split(" ")[1]);
 	  			numberOfInputs++;
 	  		 }
 	  		 
 	  		 if(st.contains("test")) {
-	  			if(stub.isPresent(st.split(" ")[0]))
+	  			numberOfTestInputs++;
+	  			if(stub.isPresent(st.split(" ")[1]))
 	  				falsePositiveCount++;
 	  			 
 	  		 }
 	  		 if(st.contains("reset")) {
-	  			 
+	  			 stub.reset();
 	  		 }
 	  	 }
 	  	
 	    String fileContent =  "\nFalse Positive Count : " + falsePositiveCount;
 	
-	    fileContent += "\nFalse Positive Rate Report: \n When number of input " + numberOfInputs;
-	      float falsPositiveRate = (falsePositiveCount/numberOfInputs) * 100;
-	      fileContent += "\nFalse Positive Rate is : " +falsPositiveRate + "% ";
+	    fileContent += "\nFalse Positive Rate Report: \n When number of input " + numberOfInputs + " Test Input size is : "+numberOfTestInputs
+	    		;
+	 
+	    float falsPositiveRate = (falsePositiveCount/numberOfTestInputs);
+	      fileContent += "\nFalse Positive Rate is : " +falsPositiveRate + " ";
 	  	 
 		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 		writer.write(fileContent);
